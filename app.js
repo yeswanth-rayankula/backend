a03a8157812e9e2ea612b477758367b0a1835382
yeswanth-rayankula <yaswanthrayankula89@gmail.com>
2024-08-06 14:12:08 +0530

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { dbconnection } from "./database/dbconnection.js";
 import messagerouter from "./router/messagerouter.js";
-
+import cloudinary from "cloudinary";
 import userRouter from "./router/userRouter.js";
 import appointmentRouter from "./router/appointmentRouter.js";
 const app=express();
@@ -20,6 +20,14 @@ app.use(
 
     })
 );
+
+cloudinary.v2.config({
+    cloud_name: process.env.cloudinary_cloud_name,
+    api_key:process.env.cloudinary_api_key,
+    api_secret:process.env.cloudinary_api_secret,
+
+})
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -38,4 +46,8 @@ app.use("/api/v1/user",userRouter);
 app.use("/api/v1/appointment",appointmentRouter);
 dbconnection();
 
-export default app;
+const port = process.env.PORT ; 
+
+app.listen(port,()=>{
+     console.log("deploy successful");
+});
